72c6a10fe5a18751625772b00c54a854215132bc
lethic <lethiic@gmail.com>
2015-09-25 17:26:25 -0700

    init

---
+++ examples/build/CMakeFiles/rvm_regression_ex.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/local_home/dlib-18.15/examples/rvm_regression_ex.cpp" "/local_home/dlib-18.15/examples/build/CMakeFiles/rvm_regression_ex.dir/rvm_regression_ex.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/local_home/dlib-18.15/examples/build/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/local_home/dlib-18.15/dlib/.."
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
